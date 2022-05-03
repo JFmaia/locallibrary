dde400e4b29114bbc9b23d6ff6fd367ff62d1cdf
JFmaia <jfmaia741@gmail.com>
2022-05-03 08:33:31 -0300

    Criando model do Genero.

--- locallibrary/catalog/models.py
+++ locallibrary/catalog/models.py
@@ -1,3 +1,8 @@
 from django.db import models
 
-# Create your models here.
+class Genre(models.Model):
+    #representa o genero do livro!
+    name = models.CharField(max_length=200, help_text='Digite um genero do livro (e.g. Science Fiction)')
+
+    def __str__(self):
+        return self.name
