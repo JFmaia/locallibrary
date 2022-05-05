60a3e559aa3c60e2f21cb0fcdb0ee1f8e049470f
JFmaia <jfmaia741@gmail.com>
2022-05-05 10:50:30 -0300

    Registrando os models no admin!

--- locallibrary/catalog/admin.py
+++ locallibrary/catalog/admin.py
@@ -1,3 +1,9 @@
 from django.contrib import admin
+from catalog.models import Author, Genre, Book, BookInstance, Language
 
-# Register your models here.
+# Registrando os odelos aqui.
+admin.site.register(Book)
+admin.site.register(Author)
+admin.site.register(Genre)
+admin.site.register(BookInstance)
+admin.site.register(Language)
