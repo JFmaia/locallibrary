9b2df2fe2efca20b8aad18e91c2dea3bf8450f98
JFmaia <jfmaia741@gmail.com>
2022-05-06 08:53:34 -0300

    Criando primeira view index.

--- locallibrary/catalog/views.py
+++ locallibrary/catalog/views.py
@@ -1,3 +1,24 @@
 from django.shortcuts import render
 
-# Create your views here.
+def index(request):
+    """Função view da home page do site."""
+
+    # Pegando dados dos modelos book, BookInstance e author!
+    num_books = Book.objects.all().count()
+    num_instances = BookInstance.objects.all().count()
+
+    # Livros disponiveis (status = 'd')
+    num_instances_available = BookInstance.objects.filter(status__exact='d').count()
+
+    # A funcionalidade All() é implementada por default.
+    num_authors = Author.objects.count()
+
+    context = {
+        'num_books': num_books,
+        'num_instances': num_instances,
+        'num_instances_available': num_instances_available,
+        'num_authors': num_authors,
+    }
+
+    # Renderize o modelo HTML index.html com os dados na variável de contexto
+    return render(request, 'index.html', context=context)
