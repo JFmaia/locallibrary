c64c10a39577834453cee37aa86dc746b3bee75c
JFmaia <jfmaia741@gmail.com>
2022-05-09 10:46:47 -0300

    Atualizando o model BookInstance.
    Adicionando um user para a instancia de livro.

--- locallibrary/catalog/models.py
+++ locallibrary/catalog/models.py
@@ -1,6 +1,8 @@
 from django.db import models
 from django.urls import reverse # Usado para gerar URLs invertendo os padrões de URL do Django
 import uuid # Required for unique book instances
+from django.contrib.auth.models import User
+from datetime import date
 
 class Genre(models.Model):
     #representa o genero do livro!
@@ -45,6 +47,7 @@ class BookInstance(models.Model):
     book = models.ForeignKey('Book', on_delete=models.SET_NULL, null=True)
     imprint = models.CharField(max_length=200)
     due_back = models.DateField(null=True, blank=True)
+    borrower = models.ForeignKey(User, on_delete=models.SET_NULL, null=True, blank=True)
 
     LOAN_STATUS = (
         ('m', 'Manutenção'),
@@ -68,6 +71,12 @@ class BookInstance(models.Model):
     def __str__(self):
         """Retorna uma string que representa o livro."""
         return f'{self.id} ({self.book.title})'
+    
+    @property
+    def is_overdue(self):
+        if self.due_back and date.today() > self.due_back:
+            return True
+        return False
 
 class Author(models.Model):
     """Esse model representa o autor do livro"""
