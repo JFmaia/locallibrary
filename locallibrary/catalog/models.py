9de48885525ee20c902197a7e58d843b58c60fbf
JFmaia <jfmaia741@gmail.com>
2022-05-10 18:21:43 -0300

    Criando permisssão na classe Meta
    Permissão esta para ser usada pelos blibliotecários

--- locallibrary/catalog/models.py
+++ locallibrary/catalog/models.py
@@ -67,6 +67,7 @@ class BookInstance(models.Model):
 
     class Meta:
         ordering = ['due_back']
+        permissions = (("can_mark_returned", "Set book as returned"),)
 
     def __str__(self):
         """Retorna uma string que representa o livro."""
