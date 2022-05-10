de43d9b724ea4cc17686c7be1f2a006783485b62
JFmaia <jfmaia741@gmail.com>
2022-05-10 08:15:36 -0300

    Ajustes de espaço
    Crie dois espaços.

--- locallibrary/catalog/urls.py
+++ locallibrary/catalog/urls.py
@@ -10,3 +10,4 @@ urlpatterns = [
     path('author/<int:pk>', views.AuthorDetailView.as_view(), name='author-detail'),
     path('mybooks/', views.LoanedBooksByUserListView.as_view(), name='my-borrowed'),
 ]
+
